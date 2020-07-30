26881fadd059debb3d568633034e3a6200bad03c
francisco nicolas angulo estay <fangulo@lacadenaretail.com>
2020-07-30 12:07:41 -0500

    cambio de estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
