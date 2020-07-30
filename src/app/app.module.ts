aa4c31548a7dc6bfdd9a68c9d36669b97a88216e
francisco nicolas angulo estay <fangulo@lacadenaretail.com>
2020-07-30 15:24:35 -0500

    se crea base nueva para front End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { ChatComponent } from './components/chat/chat.component';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
   ],
   imports: [FormsModule,
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
