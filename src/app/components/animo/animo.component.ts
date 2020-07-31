87f9b7413da2a604f370d21cec5b9143fbdaa1ba
francisco nicolas angulo estay <fangulo@lacadenaretail.com>
2020-07-31 00:13:30 -0500

    se agrega nuevas secciones

---
+++ src/app/components/animo/animo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animo',
+  templateUrl: './animo.component.html',
+  styleUrls: ['./animo.component.css']
+})
+export class AnimoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
